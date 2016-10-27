57e1d815266b8a44a52e404237655a43be0fd785
uxawseny <uxawseny@gmail.com>
2016-10-27 10:40:17 +0800

    username or email login

--- src/main/java/com/mavenssm/user/dao/UserDao.java
+++ src/main/java/com/mavenssm/user/dao/UserDao.java
@@ -18,26 +18,11 @@ public interface UserDao {
     /**
      * 查询所有用户
      */
-    // @Select("SELECT userName, userPhone FROM User")
     List<User> getAllUser();
 
     /**
      * 按照用户名查询是否有该用户
      */
-    // @Select("SELECT * FROM t_user WHERE user_name=#{userName}")
-   /* @Results(
-            {
-                    @Result(id = true, column = "id", property = "id"),
-                    @Result(column = "user_name", property = "userName"),
-                    @Result(column = "user_phone", property = "userPhone"),
-                    @Result(column = "user_email", property = "userEmail"),
-                    @Result(column = "user_pwd", property = "userPwd"),
-                    @Result(column = "pwd_salt", property = "pwdSalt"),
-                    @Result(column = "create_time", property = "createTime"),
-                    @Result(column = "modify_time", property = "modifyTime"),
-                    @Result(column = "is_delete", property = "isDelete")
-            })*/
-
     User getUserByName(String userName);
 
     /**
