962784c78d05bcfce22ddada0d78bb9ded02152d
uxawseny <uxawseny@gmail.com>
2016-10-27 13:16:47 +0800

    username or email login

--- src/main/java/com/mavenssm/user/controller/UserController.java
+++ src/main/java/com/mavenssm/user/controller/UserController.java
@@ -6,8 +6,6 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
